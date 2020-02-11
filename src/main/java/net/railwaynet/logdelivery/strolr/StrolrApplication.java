8cf995cf8a2bb0605f082c0a4fa9fe422fd9c964
Andrey Sukhanov <andrey.sukhanov@ontarget-group.com>
2020-02-11 14:27:26 +0300

    Added mutual authorization and the main HTML page

--- src/main/java/net/railwaynet/logdelivery/strolr/StrolrApplication.java
+++ src/main/java/net/railwaynet/logdelivery/strolr/StrolrApplication.java
@@ -5,11 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.Arrays;
 
+@EnableWebSecurity
 @SpringBootApplication
-public class StrolrApplication {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class StrolrApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StrolrApplication.class, args);
@@ -30,4 +40,30 @@ public class StrolrApplication {
 		};
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated()
+				.and().csrf().disable()
+				.x509()
+				.subjectPrincipalRegex("CN=(.*?),")
+				.userDetailsService(userDetailsService());
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsService() {
+			@Override
+			public UserDetails loadUserByUsername(String username) {
+				if (username.equals("localhost")) {
+					return new User(username, "",
+						AuthorityUtils
+								.commaSeparatedStringToAuthorityList("ROLE_USER"));
+				} else {
+					return new User(username, "",
+							AuthorityUtils
+									.commaSeparatedStringToAuthorityList("UNKNOWN"));
+				}
+			}
+		};
+	}
 }
--- src/main/java/net/railwaynet/logdelivery/strolr/WelcomeController.java
+++ src/main/java/net/railwaynet/logdelivery/strolr/WelcomeController.java
@@ -1,14 +1,26 @@
 package net.railwaynet.logdelivery.strolr;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @RestController
 public class WelcomeController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Welcome to STROLR2!";
+    private static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @RequestMapping("/user_test")
+    public String userTest(Principal principal) {
+        UserDetails currentUser
+                = (UserDetails) ((Authentication) principal).getPrincipal();
+        return "Hello " + currentUser.getUsername()+"!";
     }
 
 }
