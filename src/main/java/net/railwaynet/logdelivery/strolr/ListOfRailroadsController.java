8b6debc383cff02d08b9147a5b7c8485486d09f6
Andrey Sukhanov <andrey.sukhanov@ontarget-group.com>
2020-02-14 19:50:55 +0300

    Added railroads filter by user name, and refactored the UI code.

--- src/main/java/net/railwaynet/logdelivery/strolr/ListOfRailroadsController.java
+++ src/main/java/net/railwaynet/logdelivery/strolr/ListOfRailroadsController.java
@@ -1,14 +1,24 @@
 package net.railwaynet.logdelivery.strolr;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ListOfRailroadsController {
@@ -18,7 +28,35 @@ public class ListOfRailroadsController {
     @RequestMapping("/data.json")
     public String data(Principal principal) {
         UserDetails currentUser = (UserDetails) ((Authentication) principal).getPrincipal();
-        return "Hello " + currentUser.getUsername()+"!";
+        logger.debug(currentUser.getUsername() + " requesting the list of railroads");
+
+        Map<String, ArrayList<Map<?, ?>>> result = new HashMap<>();
+        result.put("SCAC", new ArrayList<Map<?,?>>());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Map<?, ?> map = objectMapper.readValue(new FileInputStream(ResourceUtils.getFile("classpath:data.json")), Map.class);
+            List<Map<?, ?>> scacs = (List<Map<?, ?>>) map.get("SCAC");
+            for (Map<?, ?> scac: scacs) {
+                if (scac.get("label").toString().equals(currentUser.getUsername())) {
+                    result.get("SCAC").add(scac);
+                    break;
+                }
+            }
+
+            logger.debug("JSON:" + result.get("SCAC").toString());
+        } catch (IOException e) {
+            logger.error("Can't read the list of railroads!");
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Can't read the list of railroads!", e);
+        }
+        try {
+            return objectMapper.writeValueAsString(result);
+        } catch (JsonProcessingException e) {
+            logger.error("Can't generate the list of railroads!");
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Can't generate the list of railroads!", e);
+        }
     }
 
 }
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,128 @@
+var vue_det = new Vue({
+    el: '#MainApp',
+    data: {
+        form: {
+            LocoID: null,
+            StartDate: null,
+            EndDate: null,
+            StartTime: null,
+            EndTime: null,
+            SCACMark: null
+        },
+        lstFullTree: [{
+            value: null,
+            text: 'Please select an option'
+        }],
+        show: true,
+        bLogStatus: false,
+        timerCheck: '',
+        sStatus: '',
+        timeUTC: '',
+        userName: ''
+    },
+    created: function () {
+        this.getUserName()
+        this.fetchData()
+        this.initDateTime()
+        this.timeUTC = this.getUTCTime()
+        this.timer = setInterval(this.getUTCTime, 5000)
+    },
+    methods: {
+        onSubmit(evt) {
+            evt.preventDefault()
+            this.bLogStatus = true
+            this.timer = setInterval(this.checkLogData, 1000)
+        },
+        onReset(evt) {
+            evt.preventDefault()
+            this.form.LocoID = null
+            this.form.SCACMark = null
+            this.initDateTime()
+            // Trick to reset/clear native browser form validation state
+            this.show = false
+            this.$nextTick(() => {
+                this.show = true
+            })
+        },
+        initDateTime: function () {
+            this.form.StartDate = this.formatDate()
+            this.form.EndDate = this.form.StartDate
+            this.form.StartTime = '00:01'
+            this.form.EndTime = '23:59'
+        },
+        fetchData: function () {
+            var xhr = new XMLHttpRequest()
+            var self = this
+            var dataSCAC
+
+            xhr.open('GET', 'data.json')
+            xhr.onload = function () {
+                dataSCAC = JSON.parse(xhr.responseText)
+                self.lstFullTree = self.lstFullTree.concat(dataSCAC.SCAC)
+            }
+            xhr.send()
+        },
+        getUserName: function () {
+            var xhr = new XMLHttpRequest()
+            var self = this
+            var dataUser
+
+            xhr.open('GET', 'user.json')
+            xhr.onload = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        dataUser = JSON.parse(xhr.responseText)
+                        self.userName = dataUser.userName
+                    } else {
+                        self.userName = ''
+                        console.error('error - ' + xhr.statusText);
+                    }
+                }
+            }
+            xhr.send()
+        },
+        checkLogData: function () {
+            this.sStatus = this.sStatus + ' ping '
+        },
+        cancelLogDataCheck() {
+            clearInterval(this.timerCheck)
+        },
+        formatDate: function () {
+            var d = new Date(),
+                month = '' + (d.getMonth() + 1),
+                day = '' + d.getDate(),
+                year = d.getFullYear();
+
+            if (month.length < 2)
+                month = '0' + month;
+            if (day.length < 2)
+                day = '0' + day;
+
+            return [year, month, day].join('-');
+        },
+
+        getUTCTime: function () {
+            // var str = 'AM'
+            var d = new Date();
+            var hr = d.getUTCHours();
+            var min = d.getUTCMinutes();
+            var res;
+
+            // if (hr == 0) {
+            //     hr = 12
+            // } else
+            // if (hr > 11) {
+            //     hr = hr - 11
+            //     str = 'PM'
+            // }
+            if (hr < 10) hr = '0' + hr;
+            if (min < 10) min = '0' + min;
+
+            res = hr + ":" + min // + " " + str
+            this.timeUTC = res
+            return res
+
+        }
+
+    }
+});
