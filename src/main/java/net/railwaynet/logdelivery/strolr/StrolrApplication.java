97d519dbf8961447db9190c92b01fd392a30e0b8
Andrey Sukhanov <andrey.sukhanov@ontarget-group.com>
2020-11-24 18:05:11 +0300

    Made application implements WebMvcConfigurer

--- src/main/java/net/railwaynet/logdelivery/strolr/StrolrApplication.java
+++ src/main/java/net/railwaynet/logdelivery/strolr/StrolrApplication.java
@@ -22,7 +22,7 @@ import java.sql.SQLException;
 @EnableWebSecurity
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class StrolrApplication extends WebSecurityConfigurerAdapter {
+public class StrolrApplication extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StrolrApplication.class, args);
