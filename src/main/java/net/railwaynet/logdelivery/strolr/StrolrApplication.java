7ff044a3c3699cbac0af30167defef464b795b6b
Andrey Sukhanov <you@example.com>
2021-03-05 21:46:05 +0300

    Added whitelist of CORS origins and limited access to all urls

--- src/main/java/net/railwaynet/logdelivery/strolr/StrolrApplication.java
+++ src/main/java/net/railwaynet/logdelivery/strolr/StrolrApplication.java
@@ -36,75 +36,65 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackageClasses = StrolrApplication.class)
 public class StrolrApplication extends KeycloakWebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StrolrApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StrolrApplication.class, args);
+    }
 
-	@Autowired
-	private StatusesService statusesService;
+    @Autowired
+    private StatusesService statusesService;
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-		auth.authenticationProvider(keycloakAuthenticationProvider);
-	}
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
 
-	@Bean
-	public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
-		return new KeycloakSpringBootConfigResolver();
-	}
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
 
-	@Bean
-	@Override
-	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-	}
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
-		http.authorizeRequests()
-				.antMatchers("/roles.json", "/railroads.json", "/status-update/*").permitAll()
-				.antMatchers("/locomotives.json", "/locomotive-update.json/*").hasAnyRole("amtk.locomotive.status.reader")
-				.antMatchers("/locomotive-messages.csv", "/locomotive-messages").hasAnyRole("amtk.locomotive.message.reader")
-				.antMatchers("/data-request").hasAnyRole("amtk.locomotive.status.reader", "amtk.backoffice.status.reader", "amtk.log.status.reader")
-				.anyRequest().permitAll();
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/roles.json", "/railroads.json", "/status-update/*").hasAnyRole(
+                "amtk.locomotive.status.reader", "amtk.backoffice.status.reader", "amtk.log.status.reader",
+                "amtk.locomotive.status.reader", "amtk.locomotive.message.reader")
+                .antMatchers("/locomotives.json", "/locomotive-update.json/*").hasAnyRole("amtk.locomotive.status.reader")
+                .antMatchers("/locomotive-messages.csv", "/locomotive-messages").hasAnyRole("amtk.locomotive.message.reader")
+                .antMatchers("/data-request").hasAnyRole("amtk.locomotive.status.reader", "amtk.backoffice.status.reader", "amtk.log.status.reader")
+                .anyRequest().permitAll();
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-		};
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+        };
+    }
 
-	@Bean
-	public ApplicationRunner startDirectoryMonitorService() {
-		return args -> statusesService.monitorResponseQueue();
-	}
+    @Bean
+    public ApplicationRunner startDirectoryMonitorService() {
+        return args -> statusesService.monitorResponseQueue();
+    }
 
-	/*
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll().and().csrf().disable();
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return username -> new User(username, "",
-			AuthorityUtils
-					.commaSeparatedStringToAuthorityList(username));
-	}
-*/
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://localhost:3000", "https://strolr3.railwaynet.datasages.com/");
+            }
+        };
+    }
 
 }
